71d01fa51f9c73013a1296197f4b97cae6d5850e
keiYM <matsubara.y.ac@gmail.com>
2025-01-13 22:10:04 +0900

    機械学習機能を追加(AutoGluon)

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import ydata_profiling
 from streamlit_pandas_profiling import st_profile_report
+from autogluon.tabular import TabularDataset, TabularPredictor
 
 st.title("IBM HR dataset")
 df = pd.read_csv("WA_Fn-UseC_-HR-Employee-Attrition.csv")
@@ -10,3 +11,10 @@ st.write(df)
 if st.button("探索的データ分析"):
     pr = df.profile_report()
     st_profile_report(pr)
+
+if st.button("機械学習"):
+    train_data = TabularDataset("WA_Fn-UseC_-HR-Employee-Attrition.csv")
+    label = "Attrition"
+    predictor = TabularPredictor(label=label).fit(train_data, time_limit=60)
+    y_pred = predictor.predict(train_data.drop(columns=[label]))
+    st.write(y_pred.head())

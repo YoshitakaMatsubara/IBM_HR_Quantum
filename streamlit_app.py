544c5ac0d08d0b1482e4097986accd1f5fda0c4e
keiYM <matsubara.y.ac@gmail.com>
2025-01-23 11:24:58 +0900

    機械学習機能の停止

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import ydata_profiling
 from streamlit_pandas_profiling import st_profile_report
-from autogluon.tabular import TabularDataset, TabularPredictor
+#from autogluon.tabular import TabularDataset, TabularPredictor
 
 st.title("IBM HR dataset")
 df = pd.read_csv("WA_Fn-UseC_-HR-Employee-Attrition.csv")
